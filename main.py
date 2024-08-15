ab42d1d4df58207873af9825896e20e87af0b547
Samson <samitesfaye726@gmail.com>
2024-08-16 02:51:54 +0300

    Hang Man v2

--- main.py
+++ main.py
@@ -1,29 +1,102 @@
+#Step 4
+
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
-word_list = ['camel', 'bird', 'yellow', 'green', 'car', 'cat', 'hello', 'word', 'world', 'football', 'computer', 'mango', 'banana', 'english', 'usa', 'weapon', 'monkey', 'key', 'random', 'python', 'javascript', 'earth', 'palnet', 'cow']
+end_of_game = False
+word_list = ['camel', 'bird', 'yellow', 'green', 'car', 'cat', 'hello', 'word', 'world', 'football', 'computer', 'mango', 'banana', 'english', 'usa', 'weapon', 'monkey', 'key', 'random', 'python', 'javascript', 'earth', 'planet', 'cow']
+
 
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
 
-guess = input("Guess a letter: ").lower()
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
-
-
-# Create Empty List called display
-
+#Create blanks
 display = []
-
-for _ in range(len(chosen_word)):
+for _ in range(word_length):
     display += "_"
 
-#print(display)
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
 
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
+    #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
 
-    if letter == guess:
-        display[position] = letter
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
 
-print(display)
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
