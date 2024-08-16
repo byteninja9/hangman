3e90ceaffade9532c5f739632c90b69ebb35dbfa
Samson <samitesfaye726@gmail.com>
2024-08-16 14:46:06 +0300

    Final Version

--- main.py
+++ main.py
@@ -1,54 +1,42 @@
 import random
 
-import hangman_words
-import hangman_art
-from hangman_art import logo
+from hangman_word import word_list
+from hangman_art import logo, stages
 
-end_of_game = False
+chosen_word = random.choice(word_list)
 
 
-chosen_word = random.choice(hangman_words.word_list)
-word_length = len(chosen_word)
-
-# Create a variable called 'lives' to keep track of the number of lives left. 
 print(logo)
 lives = 6
 
 
-# Create blanks
 display = []
-for _ in range(word_length):
+for _ in range(len(chosen_word)):
     display += "_"
-
+end_of_game = False
 while not end_of_game:
-    guess = input("Guess a letter: ").lower()
+    guess = input("Guess a letter: ")
 
-    # Check guessed letter
     if guess in display:
         print(f"You've already guessed {guess}. Try again.")
         continue
-    
+
     if guess in chosen_word:
-        for position in range(word_length):
+        for position in range(len(chosen_word)):
             letter = chosen_word[position]
             if letter == guess:
                 display[position] = letter
     else:
-        # If guess is not a letter in the chosen_word, reduce 'lives' by 1.
-        # If lives go down to 0, the game should stop and it should print "You lose."
         lives -= 1
-        print(hangman_art.stages[lives])
+        print(stages[lives])
         if lives == 0:
             end_of_game = True
             print("You lose.")
             print(f"The word was {chosen_word}.")
-
-    # Join all the elements in the list and turn it into a String.
+    
     print(f"{' '.join(display)}")
-
-    # Check if the user has got all the letters.
     if "_" not in display:
         end_of_game = True
         print("You win.")
 
-    # Print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+
